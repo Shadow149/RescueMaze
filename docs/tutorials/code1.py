3aee5ab13fb1b5a4a5ed0fdf444c6ea6c9d651eb
jaehughes <jaeh2@cam.ac.uk>
2020-06-22 00:00:09 +0100

    Create code1.py

---
+++ docs/tutorials/code1.py
@@ -0,0 +1,71 @@
+from controller import Robot
+
+timeStep = 32
+max_velocity = 6.28
+
+robot = Robot()
+
+wheel_left = robot.getMotor("left wheel motor")
+wheel_right = robot.getMotor("right wheel motor")
+
+leftSensors = []
+rightSensors = []
+frontSensors = []
+
+frontSensors.append(robot.getDistanceSensor("ps7"))
+frontSensors[0].enable(timeStep)
+frontSensors.append(robot.getDistanceSensor("ps0"))
+frontSensors[1].enable(timeStep)
+
+rightSensors.append(robot.getDistanceSensor("ps1"))
+rightSensors[0].enable(timeStep)
+rightSensors.append(robot.getDistanceSensor("ps2"))
+rightSensors[1].enable(timeStep)
+
+leftSensors.append(robot.getDistanceSensor("ps5"))
+leftSensors[0].enable(timeStep)
+leftSensors.append(robot.getDistanceSensor("ps6"))
+leftSensors[1].enable(timeStep)
+
+#        [left wheel speed, right wheel speed]
+speeds = [max_velocity,max_velocity]
+
+wheel_left.setPosition(float("inf"))
+wheel_right.setPosition(float("inf"))
+
+def turn_right():
+    #set left wheel speed
+    speeds[0] = 0.6 * max_velocity
+    #set right wheel speed
+    speeds[1] = -0.2 * max_velocity
+
+def turn_left():
+    #set left wheel speed
+    speeds[0] = -0.2 * max_velocity
+    #set right wheel speed
+    speeds[1] = 0.6 * max_velocity
+
+def spin():
+    #set left wheel speed
+    speeds[0] = 0.6 * max_velocity
+    #set right wheel speed
+    speeds[1] = -0.6 * max_velocity
+
+while robot.step(timeStep) != -1:
+    speeds[0] = max_velocity
+    speeds[1] = max_velocity
+
+    for i in range(2):
+        #for sensors on the left, either
+        if leftSensors[i].getValue() > 80:
+            turn_right()
+        #for sensors on the right, either
+        elif rightSensors[i].getValue() > 80:
+            turn_left()
+    
+    #for both front sensors
+    if frontSensors[0].getValue() > 80 and frontSensors[1].getValue() > 80:
+        spin()
+
+    wheel_left.setVelocity(speeds[0])
+    wheel_right.setVelocity(speeds[1])
