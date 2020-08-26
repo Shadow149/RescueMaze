f8464966b2642c089a6035ee00304bca382ed322
Kai Junge <kaijunge@Kais-MacBook-Pro.local>
2020-08-26 13:28:21 +0100

    Added updated example code

---
+++ player_controllers/calibrateColourSensor.py
@@ -0,0 +1,49 @@
+'''
+This code allows for the colour sensor to be calibrated to match the swamp, hole, and checkpoint colour
+'''
+# Import all relevant libraries
+from controller import Robot
+
+# If you would like to view the HSV decomposition. Requires opencv installation
+viewHSV = False
+if viewHSV:
+    import numpy as np
+    import cv2
+
+timeStep = 32
+startTime = 0
+duration = 0
+
+# Create a robot instance from the imported robot class
+robot = Robot()
+
+# Declare colour sensor underneith the robot
+colour_camera = robot.getCamera("colour_sensor")
+colour_camera.enable(timeStep)
+
+
+# Avoid holes and swamps by looking at the RBG colour of the camera
+def viewColour():
+    colour = colour_camera.getImage()
+
+    print("Binary colour : ", colour)
+
+    if viewHSV: 
+        img = np.array(np.frombuffer(colour, np.uint8).reshape((colour_camera.getHeight(), colour_camera.getWidth(), 4)))
+        img[:,:,2] = np.zeros([img.shape[0], img.shape[1]])
+        hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)[0][0]
+        print("HSV : ", hsv)
+
+################################################################
+###   Main loop starts here   ##################################
+################################################################
+
+while robot.step(timeStep) != -1:
+    if (robot.getTime() - startTime) < duration:
+        pass
+    else:
+        startTime = 0
+        duration = 0
+
+        # Avoid if any tiles are detected
+        viewColour()
