d23475cbf5381cd1c91a06120e5d79b65f0bfd57
rrrobo <rrrobo@icloud.com>
2020-09-01 20:14:08 +0900

    python2 is not supported

--- player_controllers/ExamplePlayerController_updated.py
+++ player_controllers/ExamplePlayerController_updated.py
@@ -1,12 +1,12 @@
 '''
-Sample code that can collect heated victims and detect visual victims. Moves around the maze environment in an unstructured method. 
+Sample code that can collect heated victims and detect visual victims. Moves around the maze environment in an unstructured method.
 '''
 # Import all relevant libraries
 from controller import Robot
 import math
 import struct
 
-# If you would like to use the camera to detect visual victims, set usecamera to True. This requires opencv to be installed. 
+# If you would like to use the camera to detect visual victims, set usecamera to True. This requires opencv to be installed.
 usecamera = False
 try:
     import cv2
@@ -15,11 +15,10 @@ try:
     print("Camera-based visual victim detection is enabled.")
 except:
     print("[WARNING] Since OpenCV and numpy is not installed, the visual victim detection is turned off. \
-        Run 'pip install opencv-python' to install OpenCV and 'pip install numpy' on your terminal/command line.\
-        If you have python2, try 'pip3 install' rather than 'pip install'. ")
+        Run 'pip install opencv-python' to install OpenCV and 'pip install numpy' on your terminal/command line.")
 
 
-# Set RGB colours of the swamp and hole to avoid them 
+# Set RGB colours of the swamp and hole to avoid them
 # These should be calibrated to match the environment
 hole_colour = b'\x1e\x1e\x1e\xff'
 swamp_colour = b'R\x89\xa7\xff'
@@ -29,7 +28,7 @@ swamp_colour = b'R\x89\xa7\xff'
 timeStep = 32
 max_velocity = 6.28
 
-# Threshold for detecting the wall 
+# Threshold for detecting the wall
 sensor_value = 0.05
 
 # Threshold for the victim being close to the wall
@@ -122,7 +121,7 @@ def detectVisualSimple(image_data, camera):
 
         # draw all contours in green and accepted ones in red
         contours, h = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        
+
         for c in contours:
             if cv2.contourArea(c) > 1000:
                 coords = list(c[0][0])
@@ -131,7 +130,7 @@ def detectVisualSimple(image_data, camera):
 
         return coords_list
 
-    else: 
+    else:
         return 0
 
 
@@ -150,14 +149,14 @@ def sendVictimMessage(victimType='N'):
         #robot type, position x cm, position z cm, victim type
         # The victim type is hardcoded as "H", but this should be changed to different victims for your program
         # Harmed = "H"
-        # Stable = "S"  
+        # Stable = "S"
         # Unharmed = "U"
         # Heated (Temperature) = "T"
         sendMessage(int(position[0] * 100), int(position[2] * 100), victimType)
         messageSent = True
 
 
-# return True/False if the robot is near an object. Change the 
+# return True/False if the robot is near an object. Change the
 def nearObject(position):
     return position < victimProximity
 
@@ -179,11 +178,11 @@ def getVisibleVictims():
 def stopAtHeatedVictim():
     global messageSent, victimDetectedGlobal
     #print(left_heat_sensor.getValue(),right_heat_sensor.getValue())
-    
+
     if left_heat_sensor.getValue() > 37 or right_heat_sensor.getValue() > 37:
         stop()
         sendVictimMessage('T')
-        
+
         print("Found heated victim!!")
         victimDetectedGlobal = True
     else:
@@ -336,7 +335,7 @@ while robot.step(timeStep) != -1:
         pass
     elif victimDetectedGlobal:
 
-        if (robot.getTime() - victimTimer) > 3: 
+        if (robot.getTime() - victimTimer) > 3:
             print("Move away from victim")
             spin()
             wheel_left.setVelocity(speeds[0])
@@ -359,7 +358,7 @@ while robot.step(timeStep) != -1:
             #for sensors on the right, either
             elif rightSensors[i].getValue() < sensor_value:
                 turn_left()
-        
+
         # See if the front distance sensors are seeing anything ahead of itself
         if frontSensors[0].getValue() < sensor_value and frontSensors[1].getValue() < sensor_value:
             spin()
@@ -367,7 +366,7 @@ while robot.step(timeStep) != -1:
         # Detect victims
         if usecamera:
             stopAtVisualVictim()
-        stopAtHeatedVictim()  
+        stopAtHeatedVictim()
 
         if victimDetectedGlobal:
             stop()
